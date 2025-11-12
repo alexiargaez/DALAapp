428641991a758a2f06ac8987a7489e13b5828987
alexiargaez <lexi.argaez@hotmail.com>
2025-11-12 11:30:00 -0600

    initial commit

---
+++ src/main/java/edu/utsa/cs3443/dalaapp/Launcher.java
@@ -0,0 +1,9 @@
+package edu.utsa.cs3443.dalaapp;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(DALAappApplication.class, args);
+    }
+}
