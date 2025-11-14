693d31d6f00adf7ad41d8d526d0297a0f37d4a8c
ToeChan102 <laurensparkelzz360@gmail.com>
2025-11-14 14:22:08 -0600

    Fleshed out the funny.fxml and FunnyScreenController class.

--- src/main/java/edu/utsa/cs3443/dalaapp/FunnyScreenController.java
+++ src/main/java/edu/utsa/cs3443/dalaapp/FunnyScreenController.java
@@ -2,5 +2,22 @@
 // covered by Lauren
 package edu.utsa.cs3443.dalaapp;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 public class FunnyScreenController {
+    @FXML
+    private Label funnyAffirmationLabel;
+
+    @FXML
+    void backButtonClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void writeFunnyClicked(ActionEvent event) {
+
+    }
+
 }
