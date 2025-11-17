4298009beaa5f87e6c4aa90c6eda31cb071d962a
alexiargaez <lexi.argaez@hotmail.com>
2025-11-16 23:03:13 -0600

    dalaappapplication new code

--- src/main/java/edu/utsa/cs3443/dalaapp/DALAappApplication.java
+++ src/main/java/edu/utsa/cs3443/dalaapp/DALAappApplication.java
@@ -4,17 +4,18 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
+import javafx.scene.Parent;
 
 public class DALAappApplication extends Application {
     @Override
-    public void start(Stage stage) throws Exception {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("welcome.fxml"));
-        stage.setScene(new Scene(fx.load(), 1000,600));
-        stage.setTitle("DALA");
+            public void start(Stage stage) throws Exception{
+    Parent root = FXMLLoader.load(
+            getClass().getResource("/edu/utsa/cs3443/dalaapp/layouts/welcome.fxml"));
+        stage.setTitle("DALA — Daily Affirmations");
+        stage.setScene(new Scene(root, 1000, 600));
         stage.show();
-    }
+}
     public static void main(String[] args){
-        launch();
+        launch(args);
     }
 }
