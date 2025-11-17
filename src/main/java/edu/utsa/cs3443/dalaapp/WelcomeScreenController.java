613c37484e44a7f6a0d6740748ac8489babac688
alexiargaez <lexi.argaez@hotmail.com>
2025-11-16 21:35:21 -0600

    menu screen updated controller

--- src/main/java/edu/utsa/cs3443/dalaapp/WelcomeScreenController.java
+++ src/main/java/edu/utsa/cs3443/dalaapp/WelcomeScreenController.java
@@ -1,6 +1,17 @@
 package edu.utsa.cs3443.dalaapp;
 
-
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
 public class WelcomeScreenController {
+    @FXML private Button start;
+
+    @FXML
+    public void initialize() {
+        AppState.ensureLoaded(); }
+
+    @FXML
+    private void onStart() {
+        SceneNav.go(start, "/edu/utsa/cs3443/dalaapp/layouts/menu.fxml", "DALA — Menu");
+    }
 }
