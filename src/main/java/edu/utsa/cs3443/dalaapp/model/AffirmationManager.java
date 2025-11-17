7906b0045943b643d39dc53f5f6a014a18ec00a3
alexiargaez <lexi.argaez@hotmail.com>
2025-11-16 22:00:50 -0600

    affirmationmanager

--- src/main/java/edu/utsa/cs3443/dalaapp/model/AffirmationManager.java
+++ src/main/java/edu/utsa/cs3443/dalaapp/model/AffirmationManager.java
@@ -6,6 +6,9 @@ import java.io.BufferedReader;
 import java.util.*;
 
 public class AffirmationManager {
+    private static final ArrayList<Affirmation> allAffirmations = new ArrayList<>();
+    private static final Random rand = new Random();
+    private static boolean populated = false;
 
 
 }
