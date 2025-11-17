d8de70e9f2c46c8cbc5737e66fb3948264002a4f
ToeChan102 <laurensparkelzz360@gmail.com>
2025-11-17 00:07:57 -0600

    Fleshed out backButtonClicked method.

--- src/main/java/edu/utsa/cs3443/dalaapp/FunnyScreenController.java
+++ src/main/java/edu/utsa/cs3443/dalaapp/FunnyScreenController.java
@@ -3,7 +3,12 @@ package edu.utsa.cs3443.dalaapp;
 import edu.utsa.cs3443.dalaapp.model.AffirmationManager;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 /**
  * Funny affirmation screen controller for the funny screen view.
@@ -11,6 +16,7 @@ import javafx.scene.control.Label;
  */
 public class FunnyScreenController {
     private AffirmationManager aManager;
+    //private Stage funnyStage;
 
     /**
      * Method to initialize a new AffirmationManager object.
@@ -29,7 +35,21 @@ public class FunnyScreenController {
      */
     @FXML
     void backButtonClicked(ActionEvent event) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("layouts/menu.fxml"));
+            Scene scene = new Scene(fxmlLoader.load());
+            Stage stage = new Stage();
 
+
+            MenuScreenController menuScreenController = fxmlLoader.getController();
+            //menuScreenController.setMainStage(stage);
+
+            stage.setScene(scene);
+            stage.show();
+
+        } catch (IOException e){
+            e.printStackTrace();
+        }
     }
 
 
